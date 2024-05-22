364ba6c4f5ff66649f183bbe635498bbcb704f39
Muhammed KAPLAN <148782684+oneoblomov@users.noreply.github.com>
2024-05-23 02:33:45 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 import mysql.connector
 import json
+from datetime import datetime
 
+from data.arayüz.quiz import Ui_quiz
 from data.arayüz.hata import Ui_hata
 from data.arayüz.Ana import Ui_MainWindow
 from data.arayüz.Giris import Ui_Giris
@@ -23,6 +25,7 @@ import sys
 import os
 
 x=0
+quizSoruSayisiDegeri= 10
 kullaniciBilgi= np.nan
 hataMsj=''
 try:
@@ -78,6 +81,133 @@ class hata(QtWidgets.QWidget):
             hataMsj=''
             self.close()
 
+#region Qsuiz
+class quiz(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_quiz()
+        self.ui.setupUi(self)
+        self.sayi= 0
+        self.sorular = self.get_soru(quizSoruSayisiDegeri)
+        self.dogruSayi= 0
+        self.yanlisSayi= 0
+        
+        self.ui.secim1.clicked.connect(lambda: self.kontrol(self.ui.secim1.text(),'secim1'))
+        self.ui.secim2.clicked.connect(lambda: self.kontrol(self.ui.secim2.text(),'secim2'))
+        self.ui.secim3.clicked.connect(lambda: self.kontrol(self.ui.secim3.text(),'secim3'))
+        self.ui.secim4.clicked.connect(lambda: self.kontrol(self.ui.secim4.text(),'secim4'))
+        self.show_question()
+        
+    def show_question(self): 
+        if self.sayi== quizSoruSayisiDegeri:
+            self.ac()
+            self.kapat()
+            self.ui.soru.setText('Quiz bitti !!!')
+            self.ui.secim1.setText('Doğru Sayısı: ')     ; self.ui.secim1.setStyleSheet('color: green')
+            self.ui.secim2.setText(str(self.dogruSayi))  ; self.ui.secim2.setStyleSheet('color: green')
+            self.ui.secim3.setText('Yanlış Sayısı: ')    ; self.ui.secim3.setStyleSheet('color: red')
+            self.ui.secim4.setText(str(self.yanlisSayi)) ; self.ui.secim4.setStyleSheet('color: red')
+            return    
+        self.ac()
+        self.ui.label_sira.setText(f'{self.sayi+1}/{quizSoruSayisiDegeri}')
+        self.ui.soru.setText(self.sorular.iloc[self.sayi, 2])
+        seçimler = np.random.choice(np.delete(np.arange(quizSoruSayisiDegeri), self.sayi), size=3, replace=False)
+
+        sik = [self.sayi, seçimler[0], seçimler[1], seçimler[2]]
+        np.random.shuffle(sik)
+
+        self.ui.secim1.setText(self.sorular.iloc[sik[0], 1])
+        self.ui.secim2.setText(self.sorular.iloc[sik[1], 1])
+        self.ui.secim3.setText(self.sorular.iloc[sik[2], 1])
+        self.ui.secim4.setText(self.sorular.iloc[sik[3], 1])
+        self.sayi += 1
+        
+    def kapat(self):
+        self.ui.secim1.setEnabled(False);self.ui.secim2.setEnabled(False);self.ui.secim3.setEnabled(False);self.ui.secim4.setEnabled(False)
+    
+    def ac(self):
+        BACKGROUND_COLOR = 'background-color: white; color: black'
+        self.ui.secim1.setEnabled(True) ; self.ui.secim1.setStyleSheet(BACKGROUND_COLOR)
+        self.ui.secim2.setEnabled(True) ; self.ui.secim2.setStyleSheet(BACKGROUND_COLOR)
+        self.ui.secim3.setEnabled(True) ; self.ui.secim3.setStyleSheet(BACKGROUND_COLOR)
+        self.ui.secim4.setEnabled(True) ; self.ui.secim4.setStyleSheet(BACKGROUND_COLOR)
+    def kontrol(self, secim, isim):
+        GREEN_COLOR = 'background-color: green'
+        RED_COLOR = 'background-color: red'
+        if secim == self.sorular.iloc[self.sayi-1, 1]:
+            if isim=='secim1':
+                self.ui.secim1.setStyleSheet(GREEN_COLOR)
+            elif isim=='secim2':
+                self.ui.secim2.setStyleSheet(GREEN_COLOR)
+            elif isim=='secim3':
+                self.ui.secim3.setStyleSheet(GREEN_COLOR)
+            elif isim=='secim4':
+                self.ui.secim4.setStyleSheet(GREEN_COLOR)
+            self.dogruSayi += 1
+
+            self.guncelle(self.sorular.iloc[self.sayi-1, 0])
+        else:
+            if isim=='secim1':
+                self.ui.secim1.setStyleSheet(RED_COLOR)
+            elif isim=='secim2':
+                self.ui.secim2.setStyleSheet(RED_COLOR)
+            elif isim=='secim3':
+                self.ui.secim3.setStyleSheet(RED_COLOR)
+            elif isim=='secim4':
+                self.ui.secim4.setStyleSheet(RED_COLOR)
+            self.yanlisSayi += 1
+            try:
+                mycursor = mydb.cursor()
+                mycursor.execute(f"UPDATE`proje`.`kelimeler` SET {kullaniciBilgi['id'][0]}_tarih= '2000-01-01',{kullaniciBilgi['id'][0]}_seviye = '{0}' WHERE İngilizce = '{self.sorular.iloc[self.sayi-1, 2]}'")
+                mydb.commit()
+            except Exception as e:
+                global hataMsj
+                hataMsj=e
+        self.kapat()
+        
+        QtCore.QTimer.singleShot(1000, self.show_question)
+        
+    def get_soru(self, count):
+        df= veri_bilgi()
+        df['tarih']= pd.to_datetime(df['tarih'],format='%Y-%m-%d')
+        df= df[df['tarih']<pd.to_datetime(f'{datetime.now().year}-{datetime.now().month}-{datetime.now().day}',format='%Y-%m-%d')]
+        df= df[df['seviye'] !=6]
+
+        df= df.sort_values("seviye",ascending=False)
+        soruSayi= count
+        sorular= df.drop(df.index)
+        for i in [5,4,3,2,1,0]:
+            if soruSayi==0:
+                return sorular
+            elif len(df[df['seviye']==i])>soruSayi:
+                sorular= pd.concat([sorular,df[df['seviye']==i].sample(soruSayi)])
+                soruSayi= 0
+            else:
+                sorular= pd.concat([sorular,df[df['seviye']==i]])
+                soruSayi-= len(df[df['seviye']==i])
+        if soruSayi==0:
+                return sorular
+    def guncelle(self,id):
+        try:
+            mycursor = mydb.cursor()
+            mycursor.execute(f"SELECT {kullaniciBilgi['id'][0]}_tarih, {kullaniciBilgi['id'][0]}_seviye FROM proje.kelimeler where id= '{id}'")
+            myresult = pd.DataFrame(mycursor.fetchall())
+            artis=myresult[1][0]
+            tarih=pd.to_datetime(f'{datetime.now().year}-{datetime.now().month}-{datetime.now().day}',format='%Y-%m-%d')
+            if   artis==0:            tarih=tarih+ pd.tseries.offsets.DateOffset(days=1)
+            elif artis==1:            tarih=tarih+ pd.tseries.offsets.DateOffset(weeks=1)
+            elif artis==2:            tarih=tarih+ pd.tseries.offsets.DateOffset(months=1)
+            elif artis==3:            tarih=tarih+ pd.tseries.offsets.DateOffset(months=3)
+            elif artis==4:            tarih=tarih+ pd.tseries.offsets.DateOffset(months=6)
+            elif artis==5:            tarih=tarih+ pd.tseries.offsets.DateOffset(years=1)
+        
+            tarih=pd.to_datetime(tarih,format='%Y-%m-%d')
+            mycursor.execute(f"UPDATE`proje`.`kelimeler` SET {kullaniciBilgi['id'][0]}_tarih= '{tarih}',{kullaniciBilgi['id'][0]}_seviye = '{artis+1}' WHERE id = '{id}'")
+            mydb.commit()
+        except Exception as e:
+            global hataMsj
+            hataMsj=e
+        
 #region Uygulama        
 class uygulama(QtWidgets.QMainWindow):
     def __init__(self):
@@ -91,8 +221,11 @@ class uygulama(QtWidgets.QMainWindow):
         self.ui.ses.clicked.connect(self.ses)
         
         self.ui.actionKelime_Ekle.triggered.connect(self.KelimeEkle)
+        self.ui.actionQuiz_i_Ba_lat.triggered.connect(self.quiz)
         
-    
+    def quiz(self):
+        self.ana_sayfa = quiz()
+        self.ana_sayfa.show()
     def KelimeEkle(self):
         self.ana_sayfa = KelimeEkle()
         self.ana_sayfa.show()
@@ -272,6 +405,7 @@ class KayitUnut(QtWidgets.QWidget):
                 mydb.commit()
                 hataMsj='Şifreler Yenilendi'
             except Exception as e:
+                global hataMsj
                 hataMsj=e
         else:
             hataMsj='kısa'
